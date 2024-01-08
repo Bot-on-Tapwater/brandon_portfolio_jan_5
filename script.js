cfa23cc904abe5905dcb55e978cad3d6a8660235
Bot-on-Tapwater <mundabrandon@outlook.com>
2024-01-08 16:59:48 +0300

    Implement about experience project and contact sections

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+/**
+ * Toggles the menu and icon by adding or removing the "open" class.
+ *
+ * @return {void} This function does not return a value.
+ */
 function toggleMenu() {
     const menu = document.querySelector(".menu-links");
     const icon = document.querySelector(".hamburger-icon");
