f987b0a62d8b46322215c442dc91d276017888a9
Bot-on-Tapwater <mundabrandon@outlook.com>
2024-02-09 11:25:45 +0300

    Add placeholder images while actual images load

--- script.js
+++ script.js
@@ -9,3 +9,21 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Get reference to the main image and placeholder
+    const images = document.querySelectorAll(".main-image, .project-img, .about-pic");
+    const placeholders = document.querySelectorAll(".placeholder");
+    images.forEach((image, index) => {
+        // Create a new Image object for each selected image
+        const imgLoader = new Image();
+
+        // Set the src attribute of the Image object to the current image's src
+        imgLoader.src = image.src
+
+        imgLoader.onload = function () {
+            placeholders[index].style.display = "none"; // Hide the placeholder
+            image.style.display = "flex"; // Show the loaded image
+        };
+    });
+});
